23f04178b3b6c2104393cb793dba84520e474969
Jonathan Ballard <asgardschosen@gmail.com>
2021-04-26 22:44:20 -0700

    test area for refamiliarization

---
+++ src/Components/BaseModule/BaseModule.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './BaseModule.css';
+
+// this will hold a bar to traverse between test components
+
+let BaseModule = props => {
+
+    return(
+        <div className='BaseModule'>
+            <h1>Different Test Components Here!</h1>
+        </div>
+    )
+
+}
+
+export default BaseModule;
---
+++ src/Components/TenTwentyEight/TenTwentyEight.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TenTwentyEight.css';
+
+let TenTwentyEight = props => {
+
+    return(
+        <div className='ten28'>
+            <p>10:28</p>
+        </div>
+    );
+}
+
+export default TenTwentyEight;
+
