a8340ad87802974c6a166468e1db319e6aa35a3f
Jonathan Ballard <asgardschosen@gmail.com>
2021-04-28 22:41:40 -0700

    added bootstrap and FA and such

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import './Components/TenTwentyEight/TenTwentyEight';
 import TenTwentyEight from './Components/TenTwentyEight/TenTwentyEight';
 import BaseModule from './Components/BaseModule/BaseModule';
-import {Router} from '@reach/router';
+import { Router } from '@reach/router';
 
 function App() {
   return (
     <div className="App">
+        
+
         <Router>
             <BaseModule path="/"></BaseModule>
             <TenTwentyEight path="/1028"></TenTwentyEight>
